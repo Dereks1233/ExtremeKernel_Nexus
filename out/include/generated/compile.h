2be221bfa1a679dbf831341a65abccead2960612
Ian Rojas <ianr3590@gmail.com>
2025-06-27 22:37:32 -0400

    Susfs 1.5.8

---
+++ out/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "aarch64"
+#define UTS_VERSION "#1 SMP PREEMPT Thu Jun 26 04:15:22 -04 2025"
+#define LINUX_COMPILE_BY "ianro"
+#define LINUX_COMPILE_HOST "Ian-Notebook"
+#define LINUX_COMPILER "Neutron clang version 18.0.0git (https://github.com/llvm/llvm-project.git ca5d34ec7186f2b5750c7e67dcb8b2d0dc865d8d), Neutron LLD 18.0.0"
